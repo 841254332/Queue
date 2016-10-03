4d90453ea4682ec89827804539b0a9a307ffa868
luojiahuan <841254332@qq.com>
2016-10-03 11:22:33 +0800

    Initial Commit

---
+++ Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by luojiahua on 16/10/3.
+//  Copyright © 2016年 luojiahua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
